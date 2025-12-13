cf17fe50820ba9c43fe925f096e91cd69c3a1360
F3rits <berardhenry2022@gmail.com>
2025-12-13 11:27:12 -0600

    Add files via upload

---
+++ game/walls.js
@@ -0,0 +1,36 @@
+export const walls = [
+  // Row 0
+  [
+    // Column 0
+    [
+      { x: 380, y: 100, width: 40, height: 400 }
+    ],
+
+    // Column 1
+    [
+      { x: 200, y: 100, width: 400, height: 40 },
+      { x: 560, y: 140, width: 40, height: 160 },
+      { x: 200, y: 300, width: 400, height: 40 },
+      { x: 200, y: 340, width: 40, height: 160 },
+      { x: 200, y: 500, width: 400, height: 40 }
+    ]
+  ],
+
+  // Row 1
+  [
+    // Column 0
+    [
+      { x: 200, y: 100, width: 400, height: 40 },
+      { x: 200, y: 300, width: 400, height: 40 },
+      { x: 200, y: 500, width: 400, height: 40 },
+      { x: 560, y: 140, width: 40, height: 360 }
+    ],
+
+    // Column 1
+    [
+      { x: 200, y: 100, width: 40, height: 240 },
+      { x: 200, y: 300, width: 400, height: 40 },
+      { x: 560, y: 100, width: 40, height: 440 }
+    ]
+  ]
+];
