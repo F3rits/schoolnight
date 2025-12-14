3e1f93559f7bf3895eadd8cf611fa43f43e043d5
F3rits <berardhenry2022@gmail.com>
2025-12-14 12:52:01 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,59 @@
 import Game from "./game/game.js";
 
-import { walls } from "./game/walls.js";
-import { loadWalls } from "./game/walls.js";
-
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth - 10;
-canvas.height = window.innerHeight - 20;
+ctx.imageSmoothingEnabled = false;
 
-loadWalls(walls.length, walls[0].length);
+const tileSize = 32;
+const worldWidth = tileSize * 28;
+const worldHeight = tileSize * 18;
+
+function resizeCanvas() {
+  const scale = Math.min(
+    window.innerWidth / worldWidth,
+    window.innerHeight / worldHeight
+  );
+
+  canvas.width = worldWidth;
+  canvas.height = worldHeight;
+
+  canvas.style.width = `${worldWidth * scale}px`;
+  canvas.style.height = `${worldHeight * scale}px`;
+}
+
+
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
 
 const game = new Game(ctx, canvas);
 
-const start = document.getElementById("start");
+document.getElementById("start").onclick = async () => {
+  document.getElementById("start").style.display = "none";
 
-start.addEventListener("click", () =>{
-start.style.visibility = "hidden";
-game.start();
-})
+  if (document.documentElement.requestFullscreen) {
+    await document.documentElement.requestFullscreen();
+  }
 
+  game.start();
+};
+
+const fullscreenBtn = document.getElementById("fullscreen");
+
+fullscreenBtn.onclick = async () => {
+  if (!document.fullscreenElement) {
+    await document.documentElement.requestFullscreen();
+  }
+};
+
+
+
+document.addEventListener("fullscreenchange", () => {
+    if(document.fullscreenElement){
+        fullscreenBtn.style.display = "none";
+    }else{
+        fullscreenBtn.style.display = "block";
+    }
+     
+});
